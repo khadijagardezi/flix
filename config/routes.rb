9c4692f7f51df49a699646979f54985a23a7992a
Khadija-Batool <khadijagardezi10@gmail.com>
2024-01-01 14:36:02 +0500

    define first action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # verb "url" => "name_of_controller#name_of_action"
+    get "movies" => "movies#index"
 end
