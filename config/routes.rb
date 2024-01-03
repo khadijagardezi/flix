cca4a322d5761ad64802174f1ee265e0aeff5d2a
Khadija-Batool <khadijagardezi10@gmail.com>
2024-01-03 23:04:36 +0500

    set root for listing all `movies`

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # verb "url" => "name_of_controller#name_of_action"
+    root "movies#index"
     get "movies" => "movies#index"
     get "movies/:id" => "movies#show"
 end
